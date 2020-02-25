20f6e98ff8729bb7dc8ca93a523aa66039aa34ff
Lujan Sanchez <mlulasan@gmail.com>
2020-02-25 20:08:39 -0300

    subo adivinar cumpleaños

--- adivinarcumpleanios.js
+++ adivinarcumpleanios.js
@@ -2,9 +2,9 @@
 
 // Crear un programa que pida adivinar tu cumpleaños ingresando por separado ingresar día, mes y año, en números, y mostrar luego si un mensaje si ha adivinado o no.
 
-const dia = Number(prompt("Ingresa mi día de cumpleaños"));
-const mes = Number(prompt("Ingresa mi mes de cumpleaños"));
-const anio = Number(prompt("Ingresa mi año de cumpleaños"));
+const dia = Number(prompt("Ingresa el día de mi cumpleaños"));
+const mes = Number(prompt("Ingresa el mes de mi cumpleaños en números"));
+const anio = Number(prompt("Ingresa mi año de nacimiento"));
 
 
 const mensaje = dia === 14 && mes === 4 && anio === 1987 && "¡Felicidades!¡Usted ha adivinado, mi cumpleaños es el 14/04/1987!" ||
